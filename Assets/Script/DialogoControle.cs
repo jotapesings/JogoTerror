89cbf197eb910b99e05421fa2c62203eee85d82f
jotapesings <57123887+jotapesings@users.noreply.github.com>
2023-12-09 20:25:46 -0300

    Dialogo da História do Jogo em Legenda + Próxima Fase

--- Assets/Script/DialogoControle.cs
+++ Assets/Script/DialogoControle.cs
@@ -1,47 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 using TMPro;
 
 public class DialogoControle : MonoBehaviour
 {
 
-    
+    [Header("Variavel para Pular a Cena")]
+    public string proximaFase;
 
-    [Header("Variaveis do Dialogo do Personagem")]
+    [Header("Variaveis do Dialogo da História")]
     [SerializeField] GameObject _text;
     [SerializeField] TMP_Text _speechText;
     [SerializeField] string[] _caixaDialogo;
     [SerializeField] int index;
     [SerializeField] private float _tempoDialogo;
 
-    [Header("Variaveis do Dialogo do Objetivo")]
-    [SerializeField] GameObject _text2;
-    [SerializeField] TMP_Text _speechText2;
-    [SerializeField] string[] _caixaDialogo2;
-    [SerializeField] int index2;
+    //[Header("Variaveis do Dialogo do Objetivo")]
+    //[SerializeField] GameObject _text2;
+    //[SerializeField] TMP_Text _speechText2;
+    //[SerializeField] string[] _caixaDialogo2;
+    //[SerializeField] int index2;
 
 
     private void Start()
     {
-        StartCoroutine(DialogoLetrasPersonagem());
+        StartCoroutine(DialogoDaHistoria());
         
     }
 
-
-    private void Update()
-    {
-        //if(_ativadorDialogo == true)
-        //{
-        //    StartCoroutine(DialogoLetrasObjetivo());
-        //}
-    }
-
-    IEnumerator DialogoLetrasPersonagem() //Caixa de Dialogo Automática
+    IEnumerator DialogoDaHistoria() //Caixa de Dialogo Automática
     {
         
-        yield return new WaitForSeconds(2f);
-
         foreach (char letter in _caixaDialogo[index])
         {
             _speechText.text += letter;
@@ -49,28 +40,41 @@ public class DialogoControle : MonoBehaviour
             
         }
 
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(8f);
         _speechText.text = null; //Deixa o texto sem nada!
         
-        if(index < 1)
+        if(index < 2)
         {
             index += 1;
-            yield return DialogoLetrasPersonagem();
+            yield return DialogoDaHistoria();
         }
 
-        StartCoroutine(DialogoLetrasObjetivo());
-
-    }
-
-
-    IEnumerator DialogoLetrasObjetivo()
-    {
-        yield return new WaitForSeconds(2f);
-        _speechText2.text = _caixaDialogo2[index2];
-        index2 += 1;
-        yield return new WaitForSeconds(5f);
-        _speechText2.text = null;
+        StartCoroutine(CarregarCena());
         
+
     }
+
+
+    IEnumerator CarregarCena()
+    {
+        yield return new WaitForSeconds(1f);
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(proximaFase);
+        yield return null;
+        //while (!asyncOperation.isDone)
+        //{
+        //    this.barraProgresso.value = asyncOperation.progress;
+        //    yield return null;
+        //}
+    }
+
+    //IEnumerator DialogoLetrasObjetivo()
+    //{
+    //    yield return new WaitForSeconds(2f);
+    //    _speechText2.text = _caixaDialogo2[index2];
+    //    index2 += 1;
+    //    yield return new WaitForSeconds(5f);
+    //    _speechText2.text = null;
+        
+    //}
 
 }
