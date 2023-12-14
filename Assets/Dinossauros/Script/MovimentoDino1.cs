bf7d1763b4057eaeec55dd74d46673bd483f97bb
jotapesings <57123887+jotapesings@users.noreply.github.com>
2023-12-13 23:02:08 -0300

    Inteligência artificial do Dinossauro Funcionando!

--- Assets/Dinossauros/Script/MovimentoDino1.cs
+++ Assets/Dinossauros/Script/MovimentoDino1.cs
@@ -4,9 +4,7 @@ using System;
 
 public class MovimentoDino1 : MonoBehaviour
 {
-
-    public bool _ativadorDeInimigos;
-
+    [SerializeField] Animator _anim;
     NavMeshAgent _agent;
     private static readonly System.Random random = new System.Random();
 
@@ -25,6 +23,13 @@ public class MovimentoDino1 : MonoBehaviour
 
     bool _ativaPatrulha;
 
+    float currentSpeed = 0f;
+    float smoothTime = 0.1f;
+    float velocity = 0f;
+
+    float tempoParado = 0f;  // Contador para o tempo que o dinossauro está parado
+    float intervaloParada = 5f;  // O dinossauro vai parar a cada 5 segundos
+    float duracaoParada = 2f;  // O dinossauro vai parar por 2 segundos
 
     // Start is called before the first frame update
     void Start()
@@ -38,58 +43,50 @@ public class MovimentoDino1 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(_ativadorDeInimigos == true)
-        {
-            float distanceToPlayer = Vector3.Distance(transform.position, _player.position);
+        float targetSpeed = _agent.speed;
+        currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref velocity, smoothTime);
+        _anim.SetFloat("InputX", currentSpeed);
 
-            if (distanceToPlayer <= followDistance)
+        float distanceToPlayer = Vector3.Distance(transform.position, _player.position);
+
+        if (distanceToPlayer <= followDistance)
+        {
+            // Verifique se há um obstáculo entre o dinossauro e o jogador
+            if (!Physics.Linecast(transform.position, _player.position, obstacleMask))
             {
-                // Verifique se há um obstáculo entre o dinossauro e o jogador
-                if (!Physics.Linecast(transform.position, _player.position, obstacleMask))
-                {
-                    _agent.SetDestination(_player.transform.position);
-                    _agent.speed = _velocidadeDino1;
-                    _ativaPatrulha = false;
-                }
-                else
-                {
-                    _ativaPatrulha = true;
-                }
+                _agent.SetDestination(_player.position);
+                _agent.speed = _velocidadeDino1;
+                _ativaPatrulha = false;
             }
-            else if (distanceToPlayer > stopFollowDistance)
+            else
             {
                 _ativaPatrulha = true;
             }
-
-            if (_ativaPatrulha == true && _agent.remainingDistance < 20f)
-            {
-                _agent.SetDestination(_pos[index].position);
-                _agent.speed = 2.5f;
-            }
         }
-        else
+        else if (distanceToPlayer > stopFollowDistance)
         {
-            _agent.speed = 0f;
+            _ativaPatrulha = true;
         }
 
-
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag("posicao"))
+        if (_ativaPatrulha && !_agent.pathPending && _agent.remainingDistance <= _agent.stoppingDistance)
         {
-            if (index < _pos.Length)
-            {
-                int aleatorio = random.Next(0, _pos.Length); //Randomiza a posicao o Dinossauro
-                index += aleatorio;
-            }
+            tempoParado += Time.deltaTime;
 
-            if (index >= _pos.Length)
+            if (tempoParado >= intervaloParada)
             {
-                index = 0;
-            }
+                _agent.isStopped = true;  // Faz o dinossauro parar
+                _agent.speed = 0f;  // Define a velocidade para 0 para ativar a animação de "parado"
 
+                if (tempoParado >= intervaloParada + duracaoParada)
+                {
+                    _agent.isStopped = false;  // Faz o dinossauro começar a se mover novamente
+                    tempoParado = 0f;  // Reinicia o contador
+
+                    index = random.Next(0, _pos.Length); // Seleciona um ponto de patrulha aleatório
+                    _agent.SetDestination(_pos[index].position);
+                    _agent.speed = 2.5f;  // Define a velocidade de volta para 2.5 para ativar a animação de movimento
+                }
+            }
         }
     }
 }
